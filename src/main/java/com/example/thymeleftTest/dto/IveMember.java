08e2c53dc204d209537632b91a59f61854d0bd2c
ravvitkim <mirkim6086@naver.com>
2025-08-04 16:52:23 +0900

    model

---
+++ src/main/java/com/example/thymeleftTest/dto/IveMember.java
@@ -0,0 +1,15 @@
+package com.example.thymeleftTest.dto;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IveMember {
+    private String memberName;
+    private int age;
+    private String address;
+}
